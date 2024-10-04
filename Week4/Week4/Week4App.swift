cd0154d53123ba6bf6e5d62a94722ee16e605052
JindiChai <118403636+JindiChai@users.noreply.github.com>
2024-10-03 23:31:30 -0400

    Week4

---
+++ Week4/Week4/Week4App.swift
@@ -0,0 +1,17 @@
+//
+//  Week4App.swift
+//  Week4
+//
+//  Created by Jindi Chai on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Week4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
